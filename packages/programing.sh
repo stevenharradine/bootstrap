864111537a97f21dd2be74b4722656ef142cedcc
Steven Harradine <stevenharradine@users.noreply.github.com>
2017-12-07 11:09:59 -0500

    install node 8

--- packages/programing.sh
+++ packages/programing.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
 apt-get install \
 	git \
 	golang \
 	ansible \
-	python-pip
+	python-pip \
+	nodejs
 
 # AWS CLI
 pip install --upgrade
